064432b99139a2c115367135a66bf7629529fa97
Szabó Csaba <szabo.csaba.1981@gmail.com>
2021-02-16 21:58:26 +0100

    Cleaning

--- buttons.py
+++ buttons.py
@@ -10,20 +10,20 @@ input_field.grid(row = 0, column = 0, pady = 5, padx = 5, ipady = 10)
 input_field.insert(0, "user input ")
 
 
-btns_frame = tk.Frame(calculator, width = 300, height = 300)
+btns_frame = tk.Frame(calculator, width = 10, height = 10)
 btns_frame.pack()
 
 
-# Generate 4 * 4 field 
+# Generate a 4 * 4 field
 lista = [7, 8, 9, "/" , 4, 5, 6, "*", 1, 2, 3, "+", "=", 0, ".", "-"]
 lol = 0
 while lol <= len(lista):
     i = 0
     for r in range(4):
         for c in range(4):
-            tk.Button(btns_frame, width = 10, height = 5, text=(lista[i]),
-                borderwidth=1 ).grid(row=r,column=c)
-            print(lista[i])
+            tk.Button(btns_frame, width = 10, height = 5, text = (lista[i]),
+                borderwidth = 1).grid(row=r, column=c)
+            print("lista + ", lista[i] )
             i += 1
             print(i)
     lol +=1
