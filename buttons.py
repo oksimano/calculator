2e867aa075ea67dda5f64de0c91dadb49da04671
Szabó Csaba <szabo.csaba.1981@gmail.com>
2021-02-16 21:08:27 +0100

    base graphic is ok

--- buttons.py
+++ buttons.py
@@ -1,17 +1,27 @@
 import tkinter as tk
+
 calculator = tk.Tk()
+# Need separate frames for a display, and the buttons
+input_frame = tk.Frame(calculator, width = 300, height = 300)
+input_frame.pack()
+
+input_field = tk.Entry(input_frame, width = 50 , justify= "right" )
+input_field.grid(row = 0, column = 0, pady = 5, padx = 5, ipady = 10)
+input_field.insert(0, "user input ")
 
 
-tk.Entry(calculator, text= "yehha").grid(row = 0)
+btns_frame = tk.Frame(calculator, width = 300, height = 300)
+btns_frame.pack()
 
+
+# Generate 4 * 4 field 
 lista = [7, 8, 9, "/" , 4, 5, 6, "*", 1, 2, 3, "+", "=", 0, ".", "-"]
 lol = 0
 while lol <= len(lista):
     i = 0
-    
     for r in range(4):
         for c in range(4):
-            tk.Button(calculator, text=(lista[i]),
+            tk.Button(btns_frame, width = 10, height = 5, text=(lista[i]),
                 borderwidth=1 ).grid(row=r,column=c)
             print(lista[i])
             i += 1
--- calc.py
+++ calc.py
@@ -1,6 +1,1 @@
-import tkinter as tk
-calculator = tk.Tk()
-
-
-tk.Entry(calculator, text= "yehha").grid(row = 0)
-calculator.mainloop()
+def 
