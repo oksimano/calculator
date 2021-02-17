41b9f7c31c83f1beedcff3a0a98adc7ebc61f63f
Szabó Csaba <szabo.csaba.1981@gmail.com>
2021-02-17 13:30:00 +0100

    While remove

--- buttons.py
+++ buttons.py
@@ -1,31 +1,10 @@
-import tkinter as tk
-
-calculator = tk.Tk()
-# Need separate frames for a display, and the buttons
-input_frame = tk.Frame(calculator, width = 300, height = 300)
-input_frame.pack()
-
-input_field = tk.Entry(input_frame, width = 50 , justify= "right" )
-input_field.grid(row = 0, column = 0, pady = 5, padx = 5, ipady = 10)
-input_field.insert(0, "user input ")
+class Dictionary:
+    def __init__(self, r,c,i):
+        
+        self.row = r
+        self.column = c 
+        self.function = i
 
 
-btns_frame = tk.Frame(calculator, width = 10, height = 10)
-btns_frame.pack()
 
 
-# Generate a 4 * 4 field
-lista = [7, 8, 9, "/" , 4, 5, 6, "*", 1, 2, 3, "+", "=", 0, ".", "-"]
-lol = 0
-while lol <= len(lista):
-    i = 0
-    for r in range(4):
-        for c in range(4):
-            tk.Button(btns_frame, width = 10, height = 5, text = (lista[i]),
-                borderwidth = 1).grid(row=r, column=c)
-            print("lista + ", lista[i] )
-            i += 1
-            print(i)
-    lol +=1
-
-calculator.mainloop()
--- calc.py
+++ calc.py
@@ -1,1 +1,56 @@
-def 
+import tkinter as tk
+import buttons
+
+calculator = tk.Tk()
+class Dictionary:
+    def __init__(self, r,c,i):
+        
+        self.row = r
+        self.column = c 
+        self.function = i
+        
+object = Dictionary
+def board():
+    # Need separate frames for a display, and the buttons
+    display_frame = tk.Frame(calculator, width = 300, height = 300)
+    display_frame.pack()
+
+    display_field = tk.Entry(display_frame, width = 50 , justify= "right" )
+    display_field.grid(row = 0, column = 0, pady = 5, padx = 5, ipady = 10)
+    display_field.insert(0, "user display ")
+
+
+    btns_frame = tk.Frame(calculator, width = 10, height = 10)
+    btns_frame.pack()
+
+    # Generate a 4 * 4 field for buttons
+    lista = [[7, 8, 9, "/" ], [4, 5, 6, "*"], [1, 2, 3, "+"], ["=", 0, ".", "-"]]
+    
+   
+  
+    for r in range(len(lista)):
+        for c in range(len(lista[r])):
+            tk.Button(btns_frame, width = 10, height = 5, text = (lista[r][c]),
+                borderwidth = 1).grid(row=r, column=c)
+            object.column = c
+            object.row = r
+            object.function = lista[r][c]
+            print(object.function)
+        
+                
+        
+
+
+def functions():
+    pass
+
+def calculations():
+    pass
+
+def answer():
+    pass
+
+
+board()
+calculator.mainloop()
+print(object)
