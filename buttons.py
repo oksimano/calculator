960166caef196d1d4e3ae55b63848fc40520531b
Szabó Csaba <szabo.csaba.1981@gmail.com>
2021-02-23 12:41:40 +0100

    click back is working

--- buttons.py
+++ buttons.py
@@ -1,10 +1,19 @@
-class Dictionary:
-    def __init__(self, r,c,i):
-        
-        self.row = r
-        self.column = c 
-        self.function = i
+list = []
+a = 12
+#def generate(a):
+#    for i in range(a):
+#        list.append(i)
+#    print(list)
 
 
+class Generate(object):
+    def __init__(self, b):
+        self.c = b
 
+    def genarate(self):
+        for i in range(a):
+            list.append(i)
+        return list
 
+lol = Generate(10)
+print(lol.genarate())
--- calc.py
+++ calc.py
@@ -4,45 +4,55 @@ import tkinter as tk
 calculator = tk.Tk()
 object = {}
 
-def onclick():
-    print("Button clicked")
-
-def board():
-    # Need separate frames for a display, and the buttons
-    display_frame = tk.Frame(calculator, width = 300, height = 300)
-    display_frame.pack()
-
-    display_field = tk.Entry(display_frame, width = 50 , justify= "right" )
-    display_field.grid(row = 0, column = 0, pady = 5, padx = 5, ipady = 10)
-    display_field.insert(0, "user display ")
-
-
-    btns_frame = tk.Frame(calculator, width = 10, height = 10)
-    btns_frame.pack()
-
-    # Generate a 4 * 4 field for buttons
-    lista = [[7, 8, 9, "/" ], [4, 5, 6, "*"], [1, 2, 3, "+"], ["=", 0, ".", "-"]]
+def click(button):
+    print("Button clicked on " + merge[button])
     
    
 
+# Need separate frames for a display, and the buttons
+display_frame = tk.Frame(calculator, width = 300, height = 300)
+display_frame.pack()
+
+display_field = tk.Entry(display_frame, width = 50 , justify= "right" )
+display_field.grid(row = 0, column = 0, pady = 5, padx = 5, ipady = 10)
+display_field.insert(0, "user_display ")
+
+
+btns_frame = tk.Frame(calculator, width = 10, height = 10)
+btns_frame.pack()
+
+btn = []
+lol = ()
+merge = {}
+# Generate a 4 * 4 field for buttons
+def board():
+    lista = [['7', '8', '9', "/" ], ['4', '5', '6', "*"], ['1', '2', '3', "+"], ["=", '0', ".", "-"]]
+    
     for r in range(len(lista)):
         for c in range(len(lista[r])):
-            tk.Button(btns_frame, width = 10, height = 5, text = (lista[r][c]), command = onclick,
-                borderwidth = 1).grid(row=r, column=c)
+            btn = tk.Button(btns_frame, width = 10, height = 5, text = (lista[r][c]), borderwidth = 1)
+            btn.grid(row=r, column=c)
+            btn["command"] = lambda btn = btn: click(btn)
             function = lista[r][c]
             row = r
             column = c
             object[function] = row, column
-     
-       
            
+            button = btn
+            ficktion = function
+            merge[button] = ficktion
+    return merge[button]
+    return btn    
 
-           
+active="red"
+default_color="white"         
         
 
 
-def functions():
-    pass
+def functions(args):
+    if args == 7:
+        print("Plusz 7")
+
 
 def calculations():
     pass
@@ -51,8 +61,9 @@ def answer():
     pass
 
 
-board()
 
-print(object)
+board()
+print(merge)
+functions('object')
 
 calculator.mainloop()
